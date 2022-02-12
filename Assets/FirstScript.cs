e9c3305bcdaef765297618c8e04322070cdf54fd
HGP44 <hgp44@thes-air-6.lan>
2022-02-12 12:38:11 -0800

    Unity Variables

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = "Quincy";
+        string teammates = "Simon, Sadat, Isaac, Vijay, Khalil, Jonathon";
+        string message = "I'm here to help";
+
+        print ("Hello, my name is " + name + ".");
+        print ("My teammates are " + teammates + ".");
+        print ("I want my bros to know that " + message + ".");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
